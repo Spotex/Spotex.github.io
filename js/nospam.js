bf6649c205a107fcd82f5b641a1b05f0e5fe3dfa
Sebastian Potekhin <seva.potekhin@icloud.com>
2018-02-01 17:48:13 +0200

    Add files via upload

--- js/nospam.js
+++ js/nospam.js
@@ -1,16 +1,13 @@
-$(function noSpam(){
-	$('.cbalink').css('display','none');
-});
 $(function openPreloader(){
 	$('.l_preloader').animate({
 		opacity: 1
 	},2000);
 });
 
-$(function hidePreloader(){
-	$('.l_preloader').animate({
-		opacity: 0
-	},2000);
+$(function hidePreloader(){ 
+    $.when( $('.l_preloader').animate({ opacity: 0.0 }, 2000) ).then(function(){ 
+         $('.l_preloader').hide(); 
+    });
 });
 
 
